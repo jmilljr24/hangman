d8f08507ae6aab7e2a867cf52ba75d4a2e494dd8
jmilljr24 <jmilljr24@gmail.com>
2023-03-15 13:53:20 -0400

    Add display method to game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,23 @@
-def key_word
-  word_list = File.read('words.txt').split
-  word = ''
-  word = word_list.sample until 4 < word.length && word.length < 13
-end
-
 class Game
+  attr_reader :key_clues
+
   def initialize
     @key = key_word
     @guesses_remaining = 10
-    @key_clues = ''
+    @key_clues = []
     @key.length.times { @key_clues << '_' }
     @letters_guessed = []
+  end
+
+  def key_word
+    word_list = File.read('words.txt').split
+    word = ''
+    word = word_list.sample until 4 < word.length && word.length < 13
+    word
+  end
+
+  def display
+    puts "Make your  next guess #{@key_clues.join(' ')}"
+    puts @key_clues
   end
 end
